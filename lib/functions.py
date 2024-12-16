800b55e4443af60282774e030669d7aaec15eeb8
shamim23m <shamimmohamud13@gmail.com>
2024-12-16 10:14:40 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
-
+# Function 1: Greet the programmer
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
+# Function 2: Greet a specific person by name
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
+# Function 3: Greet a specific person or use a default name if none is provided
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
+# Function 4: Add two numbers and return the sum
 def add(num1, num2):
-    pass
+    return num1 + num2
 
+# Function 5: Halve a number by dividing it by 2
 def halve(number):
-    pass
+    return number / 2
